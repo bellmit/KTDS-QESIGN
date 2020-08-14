f6a062914075a76d632955d7180544ba1702d67d
Lee YuPyeong <stoneberg73@gmail.com>
2020-08-14 18:22:15 +0900

    project init commit

---
+++ src/main/java/com/ktds/esign/KtdsQesignApplication.java
@@ -0,0 +1,13 @@
+package com.ktds.esign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtdsQesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtdsQesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktds/esign/KtdsQesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.esign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtdsQesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
